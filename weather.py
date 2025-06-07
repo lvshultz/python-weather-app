c2f30843500d651196736a6deb5b75f53105568c
lvshultz <shuuullltz@gmail.com>
2025-06-07 08:31:24 -0500

    allow user the search for weather data from multip le cities

--- weather.py
+++ weather.py
@@ -4,32 +4,40 @@ from datetime import datetime
 API_KEY = "27aba962c428bf04ada751cddae13305"
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 
-city = input("Enter the city name: ")
-params = {
-    "q": city,
-    "appid": API_KEY,
-    "units": "imperial" # or "metric" for Celcius
-}
+print("=== Weather Checker ===")
+print("Type 'exit' to stop")
 
-response = requests.get(BASE_URL, params=params)
-data = response.json()
+while True:
+    city = input("\nEnter the city name: ").strip()
+    if city.lower() == 'exit':
+        print("\nGoodbye!")
+        break
 
-if(response.status_code == 200):
-    temp = data["main"]["temp"]
-    weather = data["weather"][0]["description"]
-    humidity = data["main"]["humidity"]
-    wind = data["wind"]["speed"]
-    time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    params = {
+        "q": city,
+        "appid": API_KEY,
+        "units": "imperial" # or "metric" for Celcius
+    }
 
-    print(f"\nWeather in {city.title()} at ({time}):")
-    print(f"\nWeather in {city.title()}:")
-    print(f"Temperature: {temp}°F")
-    print(f"Condition: {weather}")
-    print(f"Humidity: {humidity}%")
-    print(f"Wind Speed: {wind} m/s")
+    response = requests.get(BASE_URL, params=params)
+    data = response.json()
 
-    with open("weather_log.txt", "a") as file:
-        file.write(f"{time} | {city.title()} | {temp}°F | {weather} | Humidity: {humidity}% | Wind: {wind} m/s\n")
-else:
-    print("\nCity not found. PLease check your spelling.")
+    if(response.status_code == 200):
+        temp = data["main"]["temp"]
+        weather = data["weather"][0]["description"]
+        humidity = data["main"]["humidity"]
+        wind = data["wind"]["speed"]
+        time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        print(f"\nWeather in {city.title()} at ({time}):")
+        print(f"\nWeather in {city.title()}:")
+        print(f"Temperature: {temp}°F")
+        print(f"Condition: {weather}")
+        print(f"Humidity: {humidity}%")
+        print(f"Wind Speed: {wind} m/s")
+
+        with open("weather_log.txt", "a") as file:
+            file.write(f"{time} | {city.title()} | {temp}°F | {weather} | Humidity: {humidity}% | Wind: {wind} m/s\n")
+    else:
+        print("\nCity not found. PLease check your spelling.")
 
